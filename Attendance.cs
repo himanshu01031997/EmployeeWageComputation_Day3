ec6021f2c97ce0a0c44852750a98b1198a853960
himanshu01031997 <hs010397@gmail.com>
2022-10-06 13:01:44 +0530

    add: added attendance of employee

---
+++ Attendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public static  class Attendance
+    {
+        public static int ispresent = 1;
+        public static void CheckAttendance()
+        {
+            Console.WriteLine("Attendance of employee");
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == 1)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation");
+            //Console.WriteLine("Welcome to Employee Wage Computation");
+            Attendance.CheckAttendance();
         }
     }
 }
