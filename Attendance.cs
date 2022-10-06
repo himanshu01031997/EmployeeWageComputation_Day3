df790da0592983fb9f4abfc55e46a3eb7af88178
himanshu01031997 <hs010397@gmail.com>
2022-10-06 13:17:27 +0530

    add:part time employee and wages

--- Attendance.cs
+++ Attendance.cs
@@ -6,34 +6,41 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputation
 {
-    public static  class DailyWages
-    {
-            // public static int ispresent = 1;
-        public static int WAGE_PER_HR = 20;
-        public static int FULL_TIME = 1;
-        public static void Wages()
+   
+        public static class ParttimeEmp
         {
-            int emphr = 0;
-            int Empwage = 0;
-
-            Console.WriteLine("Daily wages of employee");
-            Random random = new Random();
-            int empcheck = random.Next(0, 2);
-            if (empcheck == FULL_TIME)
+            // public static int ispresent = 1;
+            public static int WAGE_PER_HR = 20;
+            public static int FULL_TIME = 1;
+            public static int PART_TIME = 2;
+            public static int ABSENT = 0;
+            public static void Wages()
             {
-                Console.WriteLine("employee is fulltime");
-                emphr = 8;
+                int emphr = 0;
+                int Empwage = 0;
 
+                Console.WriteLine("Daily wages of employee");
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                if (empcheck == FULL_TIME)
+                {
+                    Console.WriteLine("employee is fulltime");
+                    emphr = 10;
+
+                }
+                else if (empcheck == PART_TIME)
+                {
+                    Console.WriteLine("employee is parttime");
+                    emphr = 8;
+
+                }
+                else
+                {
+                    Console.WriteLine("employee is absent");
+                    emphr = 0;
+                }
+                Empwage = emphr * WAGE_PER_HR;
+                Console.WriteLine("Daily wage of employee is {0}", Empwage);
             }
-            else
-            {
-                Console.WriteLine("employee is absent");
-                emphr = 0;
-
-            }
-            Empwage = emphr * WAGE_PER_HR;
-            Console.WriteLine("Daily wage of employee is {0}", Empwage);
-
         }
-    }
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             //Console.WriteLine("Welcome to Employee Wage Computation");
-            DailyWages.Wages();
+            //     DailyWages.Wages();
+            ParttimeEmp.Wages();
         }
     }
 }
