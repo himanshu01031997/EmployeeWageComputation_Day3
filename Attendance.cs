08d28ea0e4758a06ce857c7e9520ef430f22a583
himanshu01031997 <hs010397@gmail.com>
2022-10-06 13:09:14 +0530

    add:daily wages of employee

--- Attendance.cs
+++ Attendance.cs
@@ -6,22 +6,34 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputation
 {
-    public static  class Attendance
+    public static  class DailyWages
     {
-        public static int ispresent = 1;
-        public static void CheckAttendance()
+            // public static int ispresent = 1;
+        public static int WAGE_PER_HR = 20;
+        public static int FULL_TIME = 1;
+        public static void Wages()
         {
-            Console.WriteLine("Attendance of employee");
+            int emphr = 0;
+            int Empwage = 0;
+
+            Console.WriteLine("Daily wages of employee");
             Random random = new Random();
             int empcheck = random.Next(0, 2);
-            if (empcheck == 1)
+            if (empcheck == FULL_TIME)
             {
-                Console.WriteLine("employee is present");
+                Console.WriteLine("employee is fulltime");
+                emphr = 8;
+
             }
             else
             {
                 Console.WriteLine("employee is absent");
+                emphr = 0;
+
             }
+            Empwage = emphr * WAGE_PER_HR;
+            Console.WriteLine("Daily wage of employee is {0}", Empwage);
+
         }
     }
 }
